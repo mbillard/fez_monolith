74dcaffa9326c045dd1ba0baccf39ff444edd8b1
Michel Billard <michel@mbillard.com>
2012-04-17 19:22:32 -0400

    Commit of the initial rails app files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FezMonolith::Application.config.secret_token = 'e35338a8f8a6f7150e82c7a3678b747284edaf2b379c65e29a6a4f2831f83539f7356fe9f778bfbc500eabeb26a091bd046144bae6f924ceef91e46a736227ba'
