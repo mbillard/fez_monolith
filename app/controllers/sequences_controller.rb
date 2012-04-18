f5d2a283b4079ecbe1b7f48e66daddf57eba1dee
Michel Billard <michel@mbillard.com>
2012-04-18 00:29:27 -0400

    Added some safeguards against abuse.

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -5,28 +5,34 @@ class SequencesController < ApplicationController
   end
 
   def matches
-    @sequences = Sequence.where("match_count IS NOT NULL AND match_count > 0").order("MATCH_COUNT DESC").limit(10)
+    fetch_matching_sequences
   end
 
   def update
-    if params[:match]
-      s = Sequence.find(params[:sequence_id])
-      s.match_count = (s.match_count || 0) + 1
-      s.save!
-    elsif params[:wrong]
-      Sequence.where(:id => params[:sequence_ids].split(',')).each do |s|
-        s.test_count += 1
-        s.save!
-      end
-    elsif params[:unconfirm]
+    if params[:unconfirm]
       s = Sequence.find(params[:sequence_id])
       s.false_positive_count = (s.false_positive_count || 0) + 1
       s.save!
+
+      fetch_matching_sequences
+
+      render :action => :matches
+    else
+      if params[:match]
+        s = Sequence.find(params[:sequence_id])
+        s.match_count = (s.match_count || 0) + 1
+        s.save!
+      elsif params[:wrong]
+        Sequence.where(:id => params[:sequence_ids].split(',')).each do |s|
+          s.test_count += 1
+          s.save!
+        end
+      end
+
+      fetch_more_sequences
+
+      render :action => :index
     end
-
-    fetch_more_sequences
-
-    render :action => :index
   end
 
   private
@@ -41,7 +47,11 @@ class SequencesController < ApplicationController
     initial_sequence = Sequence.where(:test_count => min_test_count).find(:first, :offset => rand(nb_least_tested))
     @sequences = Sequence.where("id > ?", initial_sequence.id).limit(nb_sequences)
 
-    @matching_count = Sequence.where("match_count IS NOT NULL AND match_count > 0").count
+    @matching_count = Sequence.where("match_count IS NOT NULL AND match_count > 0 AND (false_positive_count <= match_count OR false_positive_count IS NULL)").count
+  end
+
+  def fetch_matching_sequences
+    @sequences = Sequence.where("match_count IS NOT NULL AND match_count > 0 AND (false_positive_count <= match_count OR false_positive_count IS NULL)").order("MATCH_COUNT DESC").limit(10)
   end
 
 end
