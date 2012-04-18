942902bebd855630a56858c39b062ca406a9122f
Michel Billard <michel@mbillard.com>
2012-04-18 18:26:31 -0400

    Hacked the app to always show the good sequence on the index and only allow marking it as successfull and the others as wrong.

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -11,10 +11,11 @@ class SequencesController < ApplicationController
 
   def update
     if params[:unconfirm]
-      @track.false_positive_count = (@track.false_positive_count || 0) + 1
-      @track.save!
+      # @track.false_positive_count = (@track.false_positive_count || 0) + 1
+      # @track.save!
 
-      unless @track.fraud?
+      # unless @track.fraud?
+      unless params[:sequence_id] == "24280" # the good sequence
         s = Sequence.find(params[:sequence_id])
         s.false_positive_count = (s.false_positive_count || 0) + 1
         s.save!
@@ -25,24 +26,25 @@ class SequencesController < ApplicationController
       render :action => :matches
     else
       if params[:match]
-        @track.match_count = (@track.match_count || 0) + 1
-        @track.save!
+        # @track.match_count = (@track.match_count || 0) + 1
+        # @track.save!
 
-        unless @track.fraud?
+        # unless @track.fraud?
+        if params[:sequence_id] == "24280" # the good sequence
           s = Sequence.find(params[:sequence_id])
           s.match_count = (s.match_count || 0) + 1
           s.save!
         end
       elsif params[:wrong]
-        @track.tested_count = (@track.tested_count || 0) + 1
-        @track.save!
+        # @track.tested_count = (@track.tested_count || 0) + 1
+        # @track.save!
 
-        unless @track.fraud?
-          Sequence.where(:id => params[:sequence_ids].split(',')).each do |s|
-            s.test_count += 1
-            s.save!
-          end
-        end
+        # unless @track.fraud?
+        #   Sequence.where(:id => params[:sequence_ids].split(',')).each do |s|
+        #     s.test_count += 1
+        #     s.save!
+        #   end
+        # end
       end
 
       fetch_more_sequences
@@ -67,10 +69,11 @@ class SequencesController < ApplicationController
     min_test_count = Sequence.minimum(:test_count)
     nb_least_tested = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).count - nb_sequences
 
-    initial_sequence = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).find(:first, :offset => rand(nb_least_tested))
-    @sequences = Sequence.where("id > ?", initial_sequence.id).limit(nb_sequences)
+    # initial_sequence = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).find(:first, :offset => rand(nb_least_tested))
+    initial_sequence = Sequence.find(24280) # the good sequence
+    @sequences = Sequence.where("id >= ?", initial_sequence.id).limit(nb_sequences)
 
-    @matching_count = Sequence.where("match_count IS NOT NULL AND match_count > 0 AND (false_positive_count <= match_count OR false_positive_count IS NULL)").count
+    # @matching_count = Sequence.where("match_count IS NOT NULL AND match_count > 0 AND (false_positive_count <= match_count OR false_positive_count IS NULL)").count
   end
 
   def fetch_matching_sequences
