8be9726ebc95e9f9de6b74d529ebc0d70d65b185
Michel Billard <michel@mbillard.com>
2012-04-19 21:27:05 -0400

    Reput the application in a state closer to what it was when the solution was found.

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -24,18 +24,22 @@ class SequencesController < ApplicationController
       fetch_matching_sequences
 
       render :action => :matches
-    else
-      if params[:match]
-        # @track.match_count = (@track.match_count || 0) + 1
-        # @track.save!
+    elsif params[:match]
+      # @track.match_count = (@track.match_count || 0) + 1
+      # @track.save!
 
-        # unless @track.fraud?
-        if params[:sequence_id] == "24280" # the good sequence
-          s = Sequence.find(params[:sequence_id])
-          s.match_count = (s.match_count || 0) + 1
-          s.save!
-        end
-      elsif params[:wrong]
+      # unless @track.fraud?
+      if params[:sequence_id] == "24280" # the good sequence
+        s = Sequence.find(params[:sequence_id])
+        s.match_count = (s.match_count || 0) + 1
+        s.save!
+      end
+
+      fetch_matching_sequences
+
+      render :action => :matches
+    else
+      if params[:wrong]
         # @track.tested_count = (@track.tested_count || 0) + 1
         # @track.save!
 
@@ -66,14 +70,11 @@ class SequencesController < ApplicationController
     nb_sequences = 10
 
     # Get a random set of X consecutive sequences
-    # min_test_count = Sequence.minimum(:test_count)
-    # nb_least_tested = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).count - nb_sequences
+    min_test_count = Sequence.minimum(:test_count)
+    nb_least_tested = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).count - nb_sequences
 
-    # initial_sequence = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).find(:first, :offset => rand(nb_least_tested))
-    @sequences = []
-    @sequences << Sequence.find_by_inputs("DOWN DOWN LT RT RT A UP") # the good sequence
-    @sequences << Sequence.where("id > ?", @sequences[0].id).limit(nb_sequences - 1)
-    @sequences.flatten!
+    initial_sequence = Sequence.where(:test_count => [min_test_count, min_test_count + 1]).find(:first, :offset => rand(nb_least_tested))
+    @sequences = Sequence.where("id > ?", initial_sequence.id).order("ID ASC").limit(nb_sequences)
 
     # @matching_count = Sequence.where("match_count IS NOT NULL AND match_count > 0 AND (false_positive_count <= match_count OR false_positive_count IS NULL)").count
   end
